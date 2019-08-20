39d065fedcdefcc9f8eddc1f57528b276bd232f8
dionesb <diones.breda@gmail.com>
2019-08-20 17:13:42 -0300

    Details page added.

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-// import { Container } from './styles';
+import api from '~/services/api';
 
-export default function Details() {
-  return <h1>Details</h1>;
+import {
+  Container,
+  CharacterDetails,
+  SeriesListTitle,
+  SeriesList,
+} from './styles';
+
+export default function Details({ match }) {
+  const [character, setCharacter] = useState({
+    thumbnail: { path: '', extension: '' },
+    series: { items: [] },
+  });
+
+  useEffect(() => {
+    console.tron.log(match);
+    async function loadCharacter() {
+      const response = await api.get(`characters/${match.params.id}`, {
+        params: {
+          ts: '1',
+          apikey: 'd14feaabc55ade996eeb51b7a7b57526',
+          hash: '4363dd78fbe84f0f61107fb3f916b42c',
+        },
+      });
+      // console.tron.log(response.data.data.results[0]);
+      const char = response.data.data.results[0];
+      console.tron.log(char);
+      setCharacter(char);
+    }
+    loadCharacter();
+  }, [match]);
+
+  return (
+    <Container>
+      <CharacterDetails>
+        <img
+          src={`${character.thumbnail.path}/portrait_uncanny.${character.thumbnail.extension}`}
+          alt={character.name}
+        />
+        <div>
+          <strong>{character.name}</strong>
+          <p>{character.description}</p>
+          <Link to={`/edit/${character.id}`}>Editar</Link>
+        </div>
+      </CharacterDetails>
+      <SeriesListTitle>Séries</SeriesListTitle>
+      <hr />
+      <SeriesList>
+        {character.series.items.map(item => (
+          <li>{item.name}</li>
+        ))}
+      </SeriesList>
+    </Container>
+  );
 }
+
+Details.propTypes = {
+  match: PropTypes.shape().isRequired,
+};
---
+++ src/pages/Details/styles.js
@@ -0,0 +1,75 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+
+  hr {
+    border: 2px solid #22262a;
+    margin-top: 3px;
+  }
+`;
+
+export const CharacterDetails = styled.div`
+  margin-top: 30px;
+
+  display: flex;
+
+  img {
+    border-radius: 4px;
+  }
+
+  div {
+    margin-left: 25px;
+
+    display: flex;
+    flex-direction: column;
+
+    strong {
+      font-size: 32px;
+      color: #22262a;
+    }
+
+    p {
+      margin-top: 10px;
+      font-size: 18px;
+      line-height: 26px;
+    }
+
+    a {
+      margin-top: 20px;
+      background: #e92f25;
+      font-size: 18px;
+      font-weight: bold;
+      color: #fff;
+      padding: 10px 15px;
+      border-radius: 4px;
+
+      align-self: flex-end;
+
+      &:hover {
+        background: ${darken(0.08, '#e92f25')};
+      }
+    }
+  }
+`;
+
+export const SeriesListTitle = styled.strong`
+  margin-top: 30px;
+  font-size: 32px;
+  color: #22262a;
+`;
+
+export const SeriesList = styled.ul`
+  margin-top: 10px;
+  margin-bottom: 30px;
+
+  li {
+    font-size: 18px;
+    height: 30px;
+  }
+`;
